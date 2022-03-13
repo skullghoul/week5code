4289ca2b8fb823d926fb47b27f67cc5743062591
zander <zander.ewell0759@stu.mtec.edu>
2022-03-13 12:55:55 -0600

    This is the main project for week 5

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.navItem').css('color', 'white');
+    $('#nav').css('background-color', '#145');
+    $('li:even').css('background-color', '#ccc');
+    $('li:odd').css('background-color', '#aaa')
+});
+     
+$(document).ready(function(){
+    $('input').attr("placeholder", "Zander");
+  })
